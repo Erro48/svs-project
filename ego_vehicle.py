1baf23c298b95a8b49bd717e114647fb060e4f2b
Enrico Tagliaferri <enrico.tagliaferri2@studio.unibo.it>
2025-01-16 17:34:38 +0100

    Code refactor
    Refactor variables and setup position in source code.
    Create functions for the controls (keyboard and steering wheel+pedals) and add variable simulator: Boolean to indicate if the script is running on the simulator (with joystick) or not (not enabling joystick).

--- ego_vehicle.py
+++ ego_vehicle.py
@@ -2,6 +2,7 @@ import carla, time, pygame, math, random, cv2
 import numpy as np
 import paho.mqtt.client as mqtt
 
+# Contstants #################################################################
 EGO_VEHICLE_INITIAL_LOCATION = carla.Location(x=280, y=-207.5, z=0.1) # z=0.1 used to make the car not clip with the ground
 EGO_VEHICLE_INITIAL_ROTATION = carla.Rotation(yaw=180)
 
@@ -10,17 +11,11 @@ MQTT_PORT = 1883
 MQTT_TOPIC = "svs-rcta"
 MQTT_MESSAGE = "warning: vehicle nearby"
 
-client = carla.Client('localhost', 2000)
-client.set_timeout(20.0)
+# Functions ##############################################################
 
-world = client.get_world()
-if(world.get_map().name != "Carla/Maps/Town04"):
-    world = client.load_world("Town04")
-spectator = world.get_spectator()
+def log(message, event="vehicle"):
+    print(f"[{event}] {message}")
 
-joystick = None
-
-# Functions
 def move_spectator_to(transform, distance=1.0, x=0, y=0, z=4, yaw=0, pitch=-30, roll=0):
     back_location = transform.location - transform.get_forward_vector() * distance
     
@@ -41,6 +36,10 @@ def spawn_vehicle(vehicle_index=0, spawn_index=0, pattern='vehicle.*'):
     spawn_point = world.get_map().get_spawn_points()[spawn_index]
     vehicle = world.spawn_actor(vehicle_bp, spawn_point)
     return vehicle
+
+def destroy_actors():
+    for actor in world.get_actors().filter("vehicle.*"):
+        actor.destroy()
 
 def draw_on_screen(world, transform, content='O', color=carla.Color(0, 255, 0), life_time=20):
     world.debug.draw_string(transform.location, content, color=color, life_time=life_time)
@@ -70,11 +69,10 @@ def spawn_radar(attach_to=None,
     radar = world.spawn_actor(radar_bp, transform, attach_to=attach_to)
     return radar
 
-map = world.get_map()
-
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, reason_code, properties):
-    print(f"Connected with result code {reason_code}")
+    # print(f"Connected with result code {reason_code}")
+    log(f"Connected with result code {reason_code}", "MQTT")
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     client.subscribe(MQTT_TOPIC + "/#")
@@ -96,20 +94,9 @@ def on_publish(client, userdata, mid, reason_code, properties):
         print("We could also try using a list of acknowledged mid rather than removing from pending list,")
         print("but remember that mid could be re-used !")
 
-unacked_publish = set()
-mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-mqttc.on_publish = on_publish
-mqttc.user_data_set(unacked_publish)
-
-mqttc.on_connect = on_connect
-
-mqttc.connect(MQTT_BROKER, MQTT_PORT, 60)
-mqttc.loop_start()
-
-last_message_time = time.time()
-
 def mqtt_publish(message, topic=MQTT_TOPIC, qos=1, publish_interval=1):
     global last_message_time
+    global unacked_publish
     
     current_time = time.time()
     tdelta = current_time - last_message_time
@@ -302,7 +289,8 @@ def radar_callback(radar_data, draw_radar=True, radar_point_color=carla.Color(2,
             try:
                 alarm_sound.play()
             except Exception as e:
-                print(f"Errore durante la riproduzione del suono: {e}")
+                log(f"Error during alarm sound play", "sound")
+                # print(f"Errore durante la riproduzione del suono: {e}")
 
             if screen_color_start_time == None:
                 screen_color_start_time = pygame.time.get_ticks()
@@ -353,20 +341,14 @@ def move_spectator_relative_to_vehicle(vehicle, location_offset, rotation):
     rear_camera_transform.rotation = rotation
     move_spectator_to(rear_camera_transform)
 
-    pygame.init()
-    pygame.joystick.init()
-
-    # Associa il joystick (volante e pedaliera)
-    joystick = pygame.joystick.Joystick(0)
-    joystick.init()
-
 def load_alarm_sound():
     pygame.mixer.init()
     alarm_sound = None
     try:
         alarm_sound = pygame.mixer.Sound("alarm_sound.mp3")
     except Exception as e:
-        print(f"Errore durante il caricamento del file audio: {e}")
+        # print(f"Errore durante il caricamento del file audio: {e}")
+        log(f"Errore durante il caricamento del file audio: {e}", "sound")
     return alarm_sound
 
 def screen_color(color):
@@ -382,11 +364,136 @@ def check_screen_color():
             screen_color((0, 0, 0))
             screen_color_start_time = None
 
+# === Controls =======================================
+
+def toggle_reverse_gear():
+    global reverse
+    reverse = not reverse
+    control = carla.VehicleControl(reverse=reverse)
+
+    reverse_gear_status = ("" if reverse else "dis") + "engaged"
+    log(f"Reverse gear {reverse_gear_status}")
+
+    return control
+
+def apply_control_using_joystick(joystick, vehicle, control):
+    global automatic_brake_engaged
+    global reverse
+
+    def normalize(value, deadzone=0.05):
+        """
+        Normalize both steering wheel and pedals controls.
+        """
+
+        if abs(value) < deadzone:
+            return 0.0
+        return value
+    
+    # Leggi input dal volante (es. asse 0 per sterzo)
+    steering = joystick.get_axis(0)  # Sterzo
+
+    K2 = 1.6  # 1.6
+    throttleCmd = K2 + (2.05 * math.log10(
+        -0.7 * joystick.get_axis(3) + 1.4) - 1.2) / 0.92
+    if throttleCmd <= 0:
+        throttleCmd = 0
+    elif throttleCmd > 1:
+        throttleCmd = 1
+
+    brakeCmd = 1.6 + (2.05 * math.log10(
+        -0.7 * joystick.get_axis(4) + 1.4) - 1.2) / 0.92
+    if brakeCmd <= 0:
+        brakeCmd = 0
+    elif brakeCmd > 1:
+        brakeCmd = 1
+    
+    # Normalizza e inverti valori, se necessario
+    control.steer = normalize(steering)
+    control.brake = 1 - brakeCmd      # Invertito per alcuni dispositivi
+    if not automatic_brake_engaged:
+        control.throttle = 1 - throttleCmd  # Invertito per alcuni dispositivi
+    else:
+        control.throttle = 0
+
+    for event in pygame.event.get():
+        if event.type == pygame.JOYBUTTONDOWN:
+            if event.button == 5:
+                control = toggle_reverse_gear()
+
+    vehicle.apply_control(control)
+
+def apply_control_using_keyboard():
+    global reverse
+    global running
+
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            key = event.key
+            log(f"{pygame.key.name(key)}", "key pressed")
+            if key == pygame.QUIT or key == pygame.K_ESCAPE or key == pygame.K_q:
+                running = False
+            elif key == pygame.K_r:
+                control = toggle_reverse_gear()
+                vehicle.apply_control(control)
+            elif key == pygame.K_w:
+                log("Going " + ("backward" if reverse else "foreward"))
+                control = carla.VehicleControl(throttle=0.4, reverse=reverse)
+                vehicle.apply_control(control)
+            elif key == pygame.K_s:
+                control = carla.VehicleControl(throttle=0, brake=1)
+                vehicle.apply_control(control)
+            elif key == pygame.K_c:
+                destroy_actors()
+                log(f"Actors on map destroyed. Relunch the script...", "system")
+
+######################################################################
+#                              MAIN
+######################################################################
+
+# !!! Constants are placed on top of the file !!!
+
+# Initial setup #####################################################
+
+pygame.init()
+pygame.joystick.init()
+
+client = carla.Client('localhost', 2000)
+client.set_timeout(20.0)
+
+world = client.get_world()
+if(world.get_map().name != "Carla/Maps/Town04"):
+    world = client.load_world("Town04")
+spectator = world.get_spectator()
+
+joystick = None
+
+# Variables setup ####################################################
+
+# Flag that indicates if the script is ran on the simulator
+#   needs to be set manually
+simulator = False
 screen = pygame.display.set_mode((200, 100))
 reverse = False
 automatic_brake_engaged = False
 obstacles_enabled = False
 screen_color_start_time = None
+running = True
+points = np.zeros((600, 800, 4), dtype=np.uint8)
+alarm_sound = load_alarm_sound()
+spectator_camera = carla.Location(x=-12, y=0, z=7) if simulator else carla.Location(x=0, y=0, z=5)
+
+# MQTT setup
+unacked_publish = set()
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqttc.on_publish = on_publish
+mqttc.user_data_set(unacked_publish)
+
+mqttc.on_connect = on_connect
+
+mqttc.connect(MQTT_BROKER, MQTT_PORT, 60)
+mqttc.loop_start()
+
+last_message_time = time.time()
 
 if obstacles_enabled:
     obstacles = spawn_obstacle_vehicles(
@@ -395,105 +502,40 @@ if obstacles_enabled:
         # patterns=["vehicle.mitsubishi.fusorosa", "vehicle.carlamotors.carlacola"]
     )
 
+# Vehicle configuration
 vehicle = spawn_vehicle()
-left_radar, right_radar = spawn_rear_radars(attach_to=vehicle)
-
-points = np.zeros((600, 800, 4), dtype=np.uint8)
-
-right_radar.listen(lambda image: radar_callback(image))
-left_radar.listen(lambda image: radar_callback(image))
-
-# rear_camera = spawn_camera(attach_to=vehicle, transform=carla.Transform(carla.Location(x=3, z=4), carla.Rotation(pitch=-20, yaw=180)), width=window_size[0], height=window_size[1])
-
-# Set the vehicle position
 vehicle_transform = vehicle.get_transform()
 vehicle_transform.location = EGO_VEHICLE_INITIAL_LOCATION
 vehicle_transform.rotation = EGO_VEHICLE_INITIAL_ROTATION
 vehicle.set_transform(vehicle_transform)
 
+# Radar configuration
+left_radar, right_radar = spawn_rear_radars(attach_to=vehicle)
+right_radar.listen(lambda image: radar_callback(image))
+left_radar.listen(lambda image: radar_callback(image))
+
 time.sleep(0.2)
 
-move_spectator_relative_to_vehicle(vehicle, carla.Location(x=-12, y=0, z=7), carla.Rotation(yaw=0))
-alarm_sound = load_alarm_sound()
+move_spectator_relative_to_vehicle(vehicle, spectator_camera, carla.Rotation(yaw=0))
 
-running = True
-
-# Funzione per normalizzare valori dei controlli via volante e pedaliera
-def normalize(value, deadzone=0.05):
-    if abs(value) < deadzone:
-        return 0.0
-    return value
+# Controls configuration
+if simulator:
+    # Associa il joystick (volante e pedaliera)
+    joystick = pygame.joystick.Joystick(0)
+    joystick.init()
 
 try:
-    # Controllo del veicolo
+    # Vehicle control
     control = carla.VehicleControl()
 
     while running:
         pygame.event.pump()
 
-        # Leggi input dal volante (es. asse 0 per sterzo)
-        steering = joystick.get_axis(0)  # Sterzo
-        # throttle = joystick.get_axis(3)  # Pedale acceleratore
-        # brake = joystick.get_axis(4)     # Pedale freno
+        if simulator:
+            apply_control_using_joystick(joystick, vehicle, control)
 
-        K2 = 1.6  # 1.6
-        throttleCmd = K2 + (2.05 * math.log10(
-            -0.7 * joystick.get_axis(3) + 1.4) - 1.2) / 0.92
-        if throttleCmd <= 0:
-            throttleCmd = 0
-        elif throttleCmd > 1:
-            throttleCmd = 1
+        apply_control_using_keyboard()
 
-        brakeCmd = 1.6 + (2.05 * math.log10(
-            -0.7 * joystick.get_axis(4) + 1.4) - 1.2) / 0.92
-        if brakeCmd <= 0:
-            brakeCmd = 0
-        elif brakeCmd > 1:
-            brakeCmd = 1
-        
-        # Normalizza e inverti valori, se necessario
-        control.steer = normalize(steering)
-        # control.throttle = normalize(throttle)  # Invertito per alcuni dispositivi
-        # control.brake = normalize(brake)       # Invertito per alcuni dispositivi
-        control.brake = 1 - brakeCmd      # Invertito per alcuni dispositivi
-        if not automatic_brake_engaged:
-            control.throttle = 1 - throttleCmd  # Invertito per alcuni dispositivi
-        else:
-            control.throttle = 0
-
-        # Applica il controllo al veicolo
-        vehicle.apply_control(control)
-
-        # for i in range(1, joystick.get_numaxes()):
-        #     print(f"Axe {i}: {joystick.get_axis(i)}")
-        # print("")
-
-
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                key = event.key
-                print(f"Key pressed: {pygame.key.name(key)}")
-                if key == pygame.QUIT or key == pygame.K_ESCAPE or key == pygame.K_q:
-                    running = False
-                elif key == pygame.K_r:
-                    reverse = not reverse
-                    control = carla.VehicleControl(reverse=reverse)
-                    vehicle.apply_control(control)
-                elif key == pygame.K_w:
-                    print("Going " + ("backward" if reverse else "foreward"))
-                    control = carla.VehicleControl(throttle=0.4, reverse=reverse)
-                    vehicle.apply_control(control)
-                elif key == pygame.K_s:
-                    control = carla.VehicleControl(throttle=0, brake=1)
-                    vehicle.apply_control(control)
-                elif key == pygame.K_c:
-                    for actor in world.get_actors().filter("vehicle.*"):
-                        actor.destroy() 
-            elif event.type == pygame.JOYBUTTONDOWN:
-                if event.button == 5:
-                    print("Reverse engaged")
-                    reverse = not reverse
-                    control = carla.VehicleControl(reverse=reverse)
         check_screen_color()
         world.tick()
         pygame.display.flip()
